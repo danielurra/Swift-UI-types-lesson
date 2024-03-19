96f3dfcbe5a8d6c6a5ba5c9ca7278a9c8d68468c
Daniel Urra <danielurra75@gmail.com>
2024-03-19 05:32:11 -0500

    Create types-lesson.swift

---
+++ types-lesson.swift
@@ -0,0 +1,15 @@
+// Operators can only work with operands of the same type
+let a = 12
+let b = 12.0
+//let c = a + b
+let c = Double(a) + b
+
+var name = ""
+
+var withDecimal = 12.5
+
+var name2: String = "Pepe"
+var age2: Int = 23
+
+var isActivated = true
+isActivated = false
